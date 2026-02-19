887d3e1712b81f7efc0226742b4b519ee8c19c36
Juan Diego Palomo Solorzano <juandsol18.@mac.home>
2026-02-19 14:03:22 -0500

    Fix Vite config for ES modules

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        dealer: resolve(__dirname, 'dealer.html'),
-        driver: resolve(__dirname, 'driver.html'),
-        admin: resolve(__dirname, 'admin.html')
+        main: 'index.html',
+        dealer: 'dealer.html',
+        driver: 'driver.html',
+        admin: 'admin.html'
       }
     }
   }
